ca74856fcf20a51f1f4517c4f154315a2a1b0a52
Carlos E. <cmer718@gmail.com>
2024-11-12 15:07:32 +0100

    agregado hooks06

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Memorize } from './06-memos/Memorize';
 
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+
+
+// esta parte no la pude hacer revisar luego y crear los archivos necesarios y el index.js en la carpeta
+// import { Layout } from './05-useLayoutEffect/Layout';
+
+
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// la de multiples es la de pokemon
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
@@ -12,6 +21,6 @@ import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MultipleCustomHooks />
+    <Memorize />
   </StrictMode>,
 );
